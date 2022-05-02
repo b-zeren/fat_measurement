f67163d23c0171154adb7fa6fdf376fbfad6daf9
buket <buketzeren354@gmail.com>
2022-05-03 02:01:47 +0300

    path change

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from functions import *
 
-path="/home/buket/Desktop/codes/fat_measurement/Omerleventmutluerretroperitonalsigmoidtm/33305/DICOMIMG"
+path="/home/buket/Desktop/codes/fat_measurement/Omerleventmutluerretroperitonalsigmoidtm/33305/DICOMIMG/CT229268"
 
 print("started execution")
-slice=load_singular_slice("./Omerleventmutluerretroperitonalsigmoidtm/33305/DICOMIMG/CT229455")
+
+#load and display slice
+slice=load_singular_slice(path)
 display_scans([slice.pixel_array])
 
-
+#displat it in terms of Hounsfield Units
 hu_slice=convert_to_hu(slice)
 display_scans([slice.pixel_array,hu_slice])
 
-
+#filter fat and show it
 filtered=filter_intensities(hu_slice,-150,-25)
 display_scans([filtered])
 show_hist(filtered)
 
 
 plt.close('all')
-
-
 print("Done")
 
